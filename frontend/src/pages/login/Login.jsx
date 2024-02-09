ee5e2f95b58faa7465fc468e6b1d59599b3bb011
Manish <mbb7224@gmail.com>
2024-02-09 16:31:28 +0530

    Added Login and Logout functionalities with custom hooks and added auth context

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { loading, login } = useLogin();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login(username, password);
+    };
+
     return (
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
             <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -10,7 +21,7 @@ const Login = () => {
                     <span className="text-blue-500">ChatApp</span>
                 </h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label className="label p-2">
                             <span className="text-base label-text">
@@ -21,6 +32,8 @@ const Login = () => {
                             type="text"
                             placeholder="Enter Username"
                             className="w-full input input-bordered h-10"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
                         />
                     </div>
 
@@ -34,6 +47,8 @@ const Login = () => {
                             type="password"
                             placeholder="Enter Password"
                             className="w-full input input-bordered h-10"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
 
