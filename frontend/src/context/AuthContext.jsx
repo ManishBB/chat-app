86c3df1d6bf9ecc83827b02de2f843bd8031cb98
Manish <mbb7224@gmail.com>
2024-02-10 15:03:45 +0530

    Added Message UI enhancements, functionalities, API integrations and search functionality

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,17 +2,18 @@ import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuthContext = () => {
     return useContext(AuthContext);
 };
 
 export const AuthContextProvider = ({ children }) => {
-    const [authUser, serAuthUser] = useState(
+    const [authUser, setAuthUser] = useState(
         JSON.parse(localStorage.getItem("chat-user")) || null
     );
 
     return (
-        <AuthContext.Provider value={{ authUser, serAuthUser }}>
+        <AuthContext.Provider value={{ authUser, setAuthUser }}>
             {children}
         </AuthContext.Provider>
     );
